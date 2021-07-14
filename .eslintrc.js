549bea8aefa660dcc96abc011bf8a6d6bdfa855f
maxacaplan <maxacaplan@gmail.com>
2021-07-14 19:00:07 -0300

    added sort-keys rule to ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,20 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
     '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
-    'prettier'
+    'prettier',
   ],
-  plugins: [
-  ],
+  plugins: [],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'sort-keys': [
+      'error',
+      'asc',
+      { caseSensitive: true, minKeys: 2, natural: false },
+    ],
+  },
 }
