9fae89033528aed70084dc67b96d694dd9bd70a5
typwo <typwho@gmail.com>
2021-07-17 22:37:26 -0400

    Reworked navbar for mobile menu

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,45 +1,44 @@
 <template>
-  <nav class="flex items-center flex-wrap bg-transparent border-b border-gray-300 px-5 py-3">
-    <div class="flex flex-grow flex-shrink-0">
-      <span class="p-2">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
-        </svg>        
-      </span>
-      <span class="text-2xl tracking-tight font-bold text-gray-700">speedrun.website</span>
-    </div>
-    <ul class="flex px-5 font-semibold">
-      <li class="hidden lg:inline">
-        <a class="inline-block border border-white text-purple-500 rounded hover:border-gray-200 hover:bg-purple-50 py-2 px-4" href="#">Games</a>
-      </li>
-      <li class="hidden lg:inline">
-        <a class="inline-block border border-white text-purple-500 rounded hover:border-gray-200 hover:bg-purple-50 py-2 px-4" href="#">About</a>
-      </li>      
-    </ul>
-    <ul class="flex">
-      <li class="mr-3 hidden md:inline">
-        <div class="relative mx-auto text-gray-600">
-          <input class="border border-gray-400 bg-white rounded px-4 py-2 pr-12 focus:outline-none" name="search" placeholder="Search...">
-          <button type="submit" class="absolute right-0 bottom-3 mt-5 mr-4">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-            </svg>
-          </button>
-        </div>
-      </li>
-    </ul>
-    <ul class="flex">
-      <li class="mr-3 hidden lg:inline">
-        <a class="inline-block border border-gray-200 rounded text-purple-500 py-2 px-4 hover:border-gray-200 hover:bg-purple-50" href="#">Log In</a>
-      </li>
-      <li class="mr-3 hidden lg:inline">
-        <a class="inline-block border border-purple-500 rounded bg-purple-500 text-white py-2 px-4 hover:bg-purple-700" href="#">Sign Up</a>
-      </li>
-    </ul>
-    <div class="flex lg:hidden">
-      <button class="flex items-center border rounded text-gray-600 border-gray-300 px-3 py-2 hover:text-black hover:border-black">
+  <nav class="flex flex-wrap flex-row-reverse border-b justify-between border-gray-300 px-5 py-3 lg:flex-row">
+    <div class="flex lg:hidden" @click="mobileView = !mobileView">
+      <button class="flex items-center border rounded text-gray-600 border-gray-300 px-4 py-1 hover:text-black hover:border-black hover:bg-gray-100">
         <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
       </button>
+    </div>    
+    <div class="flex flex-grow">
+      <h1 class="w-full text-lg text-center font-semibold leading-6 tracking-tight py-2 lg:text-left">speedrun.website</h1>
     </div>
+    <div class="flex">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mx-1 lg:hidden" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+      </svg>
+    </div>
+    <div class="flex w-full flex-col font-semibold lg:flex-row lg:w-auto text-center my-2 lg:my-0" v-show="mobileView">
+      <a class="text-gray-900 border border-gray-200 rounded bg-gray-200 px-3 py-2 mx-1 my-1 lg:my-0" href="#">Home</a>
+      <a class="text-gray-600 rounded px-3 py-2 mx-1 my-1 lg:my-0 hover:text-gray-800 hover:bg-gray-100" href="#">Games</a>
+      <a class="text-gray-600 rounded px-3 py-2 mx-1 my-1 lg:my-0 hover:text-gray-800 hover:bg-gray-100" href="#">About</a>
+    </div>
+    <div class="flex flex-col relative w-full px-2 my-1 lg:my-0 lg:flex-row lg:w-auto" v-show="mobileView">
+      <input class="border border-gray-400 bg-white rounded px-4 py-2 pr-12 focus:outline-none" name="search" placeholder="Search...">
+      <button type="submit" class="absolute right-6 bottom-3">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+        </svg>
+      </button>      
+    </div>
+    <div class="flex w-full flex-col lg:flex-row lg:w-auto text-center my-2 lg:my-0" v-if="mobileView">
+      <a class="text-gray-600 border border-gray-300 rounded px-3 py-2 mx-1 my-1 lg:my-0 hover:text-gray-800 hover:bg-gray-100" href="#">Log In</a>
+      <a class="text-gray-100 bg-gray-600 rounded px-3 py-2 mx-1 my-1 lg:my-0 hover:text-white hover:bg-gray-900" href="#">Sign Up</a>
+    </div>    
   </nav>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      mobileView: false,
+    }
+  }
+}
+</script>
