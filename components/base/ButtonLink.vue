6d6eba3056a1e092d1c990af89b2fadad9fe8c85
Edward Runkel <eddierunkel@gmail.com>
2021-07-22 13:05:02 -0400

    Base ButtonLink: Created the `<ButtonLink />` component

---
+++ components/base/ButtonLink.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtLink
+    class="
+      rounded
+      px-3
+      text-current
+      hover:bg-current
+    "
+    :class="classNames"
+    :to="linkTo"
+  >
+    <slot />
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    classNames: {
+      default: '',
+      type: String
+    },
+    linkTo: {
+      default: '#',
+      type: String
+    }
+  }
+};
+</script>
