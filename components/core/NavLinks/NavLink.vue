b20dffd8038d7724e0b26656779621d74c8e2e6f
Edward Runkel <eddierunkel@gmail.com>
2021-07-22 13:21:00 -0400

    Core NavLinks: Created the `<NavLinks />` component.

---
+++ components/core/NavLinks/NavLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <BaseButtonLink
+    :class-names="classNames + `
+      w-full
+      py-0 lg:py-1
+      hover:bg-gray-100
+    `"
+    :link-to="linkTo"
+  >
+    <slot />
+  </BaseButtonLink>
+</template>
+
+<script>
+export default {
+  props: {
+    classNames: {
+      default: '',
+      type: String
+    },
+    linkTo: {
+      default: '#',
+      type: String
+    }
+  }
+};
+</script>
---
+++ components/core/NavLinks/NavLinks.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="
+        flex flex-col lg:flex-row items-center
+        w-auto
+        space-y-2 lg:space-y-0
+        text-center text-black
+      "
+  >
+    <NavLink class-names="mt-1 lg:my-0">
+      Games
+    </NavLink>
+    <NavLink>
+      About
+    </NavLink>
+  </nav>
+</template>
+
+<script>
+import NavLink from './NavLink';
+
+export default {
+  components: {
+    NavLink
+  }
+}
+</script>
