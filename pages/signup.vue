15607b76bc5a013d90f4beb0b74afd1d4232cc37
maxacaplan <maxacaplan@gmail.com>
2021-07-22 12:34:26 -0300

    signup page added

---
+++ pages/signup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    id="signup"
+    class="container mx-auto p-3 flex justify-center items-center"
+  >
+    <sign-up-card @logInClick="$router.push('/login')" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import SignUpCard from '../components/SignUpCard.vue'
+
+export default Vue.extend({
+  name: 'SignUp',
+
+  components: {
+    SignUpCard,
+  },
+})
+</script>
+
+<style></style>
