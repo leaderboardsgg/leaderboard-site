a5dab31fe1f859af1cc800e483333832dd30331d
Edward Runkel <eddierunkel@gmail.com>
2021-07-22 13:20:14 -0400

    Core SignUpButton: Created the `<SignUpButton />` component.

---
+++ components/core/SignUpButton/SignUp.vue
@@ -0,0 +1,23 @@
+<template>
+  <BaseButtonLink
+    :class-names="classNames + `
+      bg-gray-100 text-opacity-70
+      py-2
+      hover:bg-black hover:text-white
+    `"
+    link-to="#"
+  >
+    Sign Up
+  </BaseButtonLink>
+</template>
+
+<script>
+export default {
+  props: {
+    classNames: {
+      default: '',
+      type: String
+    }
+  }
+};
+</script>
---
+++ components/core/SignUpButton/SignUpButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sign-up-button">
+    <SignUp class-names="lg:hidden"/>
+    <SignUp
+      class-names="
+        m-1 lg:my-0
+        hidden lg:inline
+      "
+    />
+  </div>
+</template>
+
+<script>
+import SignUp from './SignUp';
+
+export default {
+  components: {
+    SignUp
+  }
+}
+</script>
