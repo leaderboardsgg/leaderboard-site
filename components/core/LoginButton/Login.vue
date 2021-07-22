8a29d14b1aa12bf6cd0136cfee32e528717b6aef
Edward Runkel <eddierunkel@gmail.com>
2021-07-22 13:19:40 -0400

    Core LoginButton: Created the `<LoginButton />` component.

---
+++ components/core/LoginButton/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <BaseButtonLink
+    :class-names="classNames + `
+      py-2
+      hover:bg-gray-100
+    `"
+    link-to="#"
+  >
+    Log In
+  </BaseButtonLink>
+</template>
+
+<script>
+export default {
+  props: {
+    classNames: {
+      default: '',
+      type: String
+    }
+  }
+};
+</script>
---
+++ components/core/LoginButton/LoginButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login-button">
+    <Login class-names="lg:hidden"/>
+    <Login
+      class-names="
+        m-1 lg:my-0
+        hidden lg:inline
+      "
+    />
+  </div>
+</template>
+
+<script>
+import Login from './Login';
+
+export default {
+  components: {
+    Login
+  }
+}
+</script>
