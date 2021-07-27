878189ac6e93e61093509320f34847a6c2d008fd
Edward Runkel <eddierunkel@gmail.com>
2021-07-27 15:12:48 -0400

    Components ButtonLink: Added testing for the component.

---
+++ components/base/ButtonLink/ButtonLink.test.js
@@ -0,0 +1,48 @@
+import { stubbedRender } from '@/testUtils';
+import ButtonLink from './ButtonLink';
+
+describe('<ButtonLink />', () => {
+  const defaultAttrs = { class: 'custom-link' };
+  const defaultProps = { to: 'https://www.test.com/' };
+
+  it('should render without crashing', () => {
+    const { unmount } = stubbedRender(ButtonLink, {
+      attrs: defaultAttrs,
+      props: defaultProps,
+      slots: { default: 'Button' },
+    });
+
+    unmount();
+  });
+
+  it('renders with the correct <slot />', () => {
+    const { getByText } = stubbedRender(ButtonLink, {
+      attrs: defaultAttrs,
+      props: defaultProps,
+      slots: { default: 'Any%' },
+    });
+    expect(getByText('Any%')).toBeInTheDocument();
+  });
+
+  it('renders with the passed link', () => {
+    stubbedRender(ButtonLink, {
+      attrs: defaultAttrs,
+      props: defaultProps,
+    });
+    expect(document.querySelector('a').getAttribute('href')).toBe(
+      'https://www.test.com/',
+    );
+  });
+
+  it('renders with the custom classnames', () => {
+    stubbedRender(ButtonLink, {
+      attrs: {
+        ...defaultAttrs,
+        class: 'go-fast',
+      },
+      props: defaultProps,
+    });
+
+    expect(document.querySelector('.go-fast')).toBeInTheDocument();
+  });
+});
--- testUtils.js
+++ testUtils.js
@@ -1,4 +1,34 @@
+import Vue from 'vue';
+import { RouterLinkStub } from '@vue/test-utils';
 import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/vue';
+
+export const NuxtLinkStub = Vue.component('NuxtLinkStub', {
+  extends: RouterLinkStub,
+  render(h) {
+    const temp = h(
+      this.tag || 'a',
+      {
+        attrs: {
+          ...this.$vnode.data.attrs,
+          ...this.$attrs,
+          href: this.to,
+        },
+      },
+      this.$slots.default,
+    );
+
+    return temp;
+  },
+});
+
+export const stubbedRender = (Component, options) => {
+  return render(Component, {
+    stubs: { NuxtLink: NuxtLinkStub, ...options.stubs },
+    ...options,
+  });
+};
 
 export * from '@testing-library/vue';
+export * from '@vue/test-utils';
 export { userEvent as fireEvent };
