08d9bffcd23eecb7594d566bf46eee28dd36dbc4
maxacaplan <maxacaplan@gmail.com>
2021-08-07 13:17:34 -0300

    Base Input component added

---
+++ components/base/Input.vue
@@ -0,0 +1,28 @@
+<template>
+  <input class="input" v-model="model" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: ['value'],
+
+  computed: {
+    model: {
+      get(): any {
+        return this.value
+      },
+      set(value: any): void {
+        this.$emit('input', value)
+      },
+    },
+  },
+})
+</script>
+
+<style scoped>
+.input {
+  @apply border border-gray-300 rounded px-3 py-2 focus:outline-none;
+}
+</style>
