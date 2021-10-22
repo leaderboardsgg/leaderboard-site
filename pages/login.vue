aca2b9a239fee5feb95f95596783c814353fda1a
Edward Runkel <eddierunkel@gmail.com>
2021-10-22 19:33:28 -0400

    Updated login/signup page styling.

--- pages/login.vue
+++ pages/login.vue
@@ -1,16 +1,16 @@
 <template>
   <div
     id="login"
-    class="flex flex-grow container mx-auto p-3 justify-center items-start"
+    class="flex flex-grow container mx-auto p-3 justify-center items-center"
   >
     <LogInCard @signUpClick="$router.push('/signup')" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent } from '@nuxtjs/composition-api';
 
 export default defineComponent({
   name: 'Login',
-})
+});
 </script>
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,16 +1,16 @@
 <template>
   <div
     id="signup"
-    class="flex flex-grow container mx-auto p-3 justify-center items-start"
+    class="flex flex-grow container mx-auto p-3 justify-center items-center"
   >
     <SignUpCard @logInClick="$router.push('/login')" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent } from '@nuxtjs/composition-api';
 
 export default defineComponent({
   name: 'SignUp',
-})
+});
 </script>
