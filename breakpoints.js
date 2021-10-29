b2922a48f372df2603d56f7affc958f7ade15d25
Daniel Turcich <github@danielTurcich.com>
2021-10-29 12:09:53 -0400

    Various Project settings changes (#105)
    * Added .vscode folder and settings files for such, as well as a few tsconfig and package.json changes
    
    * Fixed setting to tabs instead of spaces
    
    * readded .nvmrc
    
    * Added Tailwind extension and changed breakpoints.js to module.exports because the Extension doesn't handle normal exports.

--- breakpoints.js
+++ breakpoints.js
@@ -1,8 +1,11 @@
 /* eslint-disable vue/sort-keys */
-export const screens = {
+
+const screens = {
   sm: '640px',
   md: '768px',
   lg: '1024px',
   xl: '1280px',
   '2xl': '1536px',
 };
+
+module.exports = { screens };
