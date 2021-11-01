035b5c738bb289f445081a1746d3d2708bec6c9e
CapitaineToinon <antoinesutter@gmail.com>
2021-11-01 20:14:39 +0100

    Fixed snaps and tests

--- components/Run/Run.test.ts
+++ components/Run/Run.test.ts
@@ -14,35 +14,35 @@ describe('<Run />', () => {
 describe('#formattedStanding', () => {
   it('should return the "1st" formatted standing', () => {
     const { getByText } = stubbedRender(Run, {
-      props: { standing: '1' },
+      props: { standing: 1 },
     });
 
     expect(getByText('1st')).toBeInTheDocument();
   });
   it('should return the "2nd" formatted standing', () => {
     const { getByText } = stubbedRender(Run, {
-      props: { standing: '2' },
+      props: { standing: 2 },
     });
 
     expect(getByText('2nd')).toBeInTheDocument();
   });
   it('should return the "3rd" formatted standing', () => {
     const { getByText } = stubbedRender(Run, {
-      props: { standing: '3' },
+      props: { standing: 3 },
     });
 
     expect(getByText('3rd')).toBeInTheDocument();
   });
   it('should return the "11th" formatted standing', () => {
     const { getByText } = stubbedRender(Run, {
-      props: { standing: '11' },
+      props: { standing: 11 },
     });
 
     expect(getByText('11th')).toBeInTheDocument();
   });
   it('should return the trophy', () => {
     stubbedRender(Run, {
-      props: { standing: '1' },
+      props: { standing: 1 },
     });
     expect(document.querySelector('#trophy')).toBeInTheDocument();
   });
