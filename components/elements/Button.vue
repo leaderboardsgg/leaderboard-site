775821ef14673237411e2681abf8c1a1c22f2fdd
taylorcgaines <taylorcgaines@gmail.com>
2021-11-11 13:32:54 -0700

    feat: Refactored Button

--- components/elements/Button.vue
+++ components/elements/Button.vue
@@ -1,11 +1,19 @@
 <template>
-  <button class="base-button" v-bind="$attrs" v-on="$listeners">
+  <button class="button" v-bind="$attrs" v-on="$listeners">
     <slot />
   </button>
 </template>
 
+<script>
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  name: 'Button',
+});
+</script>
+
 <style>
-.base-button {
+.button {
   @apply rounded px-3 py-2 text-current hover:bg-current;
 }
 </style>
