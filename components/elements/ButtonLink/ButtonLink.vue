b85d1788178d9877299dddb7e473aed86016c87e
taylorcgaines <taylorcgaines@gmail.com>
2021-11-11 13:10:14 -0700

    feat: Refactored Button Link

--- components/elements/ButtonLink/ButtonLink.vue
+++ components/elements/ButtonLink/ButtonLink.vue
@@ -1,8 +1,30 @@
 <template>
-  <NuxtLink class="button-link" v-bind="$attrs" v-on="$listeners">
-    <slot> Placeholder </slot>
+  <NuxtLink class="button-link" :to="url" v-bind="$attrs" v-on="$listeners">
+    <slot>Placeholder Link Text</slot>
   </NuxtLink>
 </template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  name: 'ButtonLink',
+  props: {
+    alt: {
+      type: String,
+      default: '',
+    },
+    target: {
+      type: String,
+      default: '',
+    },
+    url: {
+      type: String,
+      default: '',
+    },
+  },
+});
+</script>
 
 <style>
 .button-link {
