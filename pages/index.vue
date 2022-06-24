92bb9fc5515e0ad9260ab5197860b93567806a05
Edward Runkel <eddierunkel@gmail.com>
2022-06-23 22:44:12 -0400

    Pages: POC example of i18n usage on a page.

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
   <div class="flex grow justify-center p-5">
     <div class="p-5 w-full rounded border border-gray-300">
       <h2 class="text-2xl font-bold text-center">
-        {{ `Welcome` }}
+        {{ $t('welcome') }}
       </h2>
       <p class="text-center">This is just a primary content placeholder.</p>
     </div>
