918ef0bf99ab91333fa30a4f2fb38830660e2717
Daniel Turcich <github@danielTurcich.com>
2023-02-15 18:48:05 -0500

    Added a vite plugin to resolve Top Level await error on my machine (probably windows specific error)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { FileSystemIconLoader } from 'unplugin-icons/loaders'
 import IconResolver from 'unplugin-icons/resolver'
 import Components from 'unplugin-vue-components/vite'
 import eslintPlugin from 'vite-plugin-eslint'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 import type { UserConfigExport } from 'vitest/config'
 import { nuxtAliases } from './nuxt.config'
@@ -26,6 +27,15 @@ const ViteConfig: UserConfigExport = {
       ],
     }),
     eslintPlugin({ fix: true, cache: true, failOnWarning: true }),
+    // Needed this on my machine to prevent this erroneous error of
+    // ✘ [ERROR] Top-level await is not available in the configured
+    // target environment ("chrome87", "edge88", "es2020", "firefox78", "safari13" + 2 overrides)
+    topLevelAwait({
+      // The export name of top-level await promise for each chunk module
+      promiseExportName: '__tla',
+      // The function to generate import names of top-level await promise in each chunk module
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
   ],
   resolve: {
     alias: nuxtAliases,
