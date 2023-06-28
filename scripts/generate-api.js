c96f099859495563280a9bb95c691fc667d0fd19
Edward Runkel <eddierunkel@gmail.com>
2023-06-28 01:52:31 -0400

    Update `generate-api` script. Add new `update-api` workflow.

--- scripts/generate-api.js
+++ scripts/generate-api.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-dsiable no-console */
+/* eslint-disable no-console */
 /* eslint-disable vue/sort-keys */
 
 /*
@@ -11,13 +11,26 @@
  * path specified by the `output` argument.
  */
 
+const args = process.argv.reduce((acc, current) => {
+  if (current.includes('=')) {
+    const [key, value] = current.substring(2).split('=')
+    acc[key] = value
+  }
+
+  return acc
+}, {})
+
 require('dotenv-safe').config()
 const { resolve } = require('path')
 const { generateApi } = require('swagger-typescript-api')
+const url =
+  args?.url ?? `${process.env.BACKEND_BASE_URL}/swagger/v1/swagger.json`
+
+console.log(`Generation url: ${url}`)
 
 generateApi({
   output: resolve(process.cwd(), './lib/api'),
-  url: `${process.env.BACKEND_BASE_URL}/swagger/v1/swagger.json`,
+  url,
 
   enumNamesAsValues: true,
   extractRequestBody: true,
