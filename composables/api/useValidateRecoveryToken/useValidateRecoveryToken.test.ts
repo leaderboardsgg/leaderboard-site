d4625f47374f9e170d74796fff01dc1dabcf546d
Edward Runkel <eddierunkel@gmail.com>
2024-02-08 21:07:26 -0500

    Add basic missing composable tests

---
+++ composables/api/useValidateRecoveryToken/useValidateRecoveryToken.test.ts
@@ -0,0 +1,22 @@
+import { useValidateRecoveryToken } from '.'
+
+const mockSuccessRecoverDetail = vi.fn(() => Promise.resolve({ ok: true }))
+
+describe('useValidateRecoveryToken', () => {
+  describe('when everything is successful', () => {
+    const token = 'recoveryToken'
+
+    it('validates the recovery token', async () => {
+      vi.mock('lib/api/Account', () => ({
+        Account: function Account() {
+          this.recoverDetail = mockSuccessRecoverDetail
+        },
+      }))
+
+      await useValidateRecoveryToken(token)
+
+      expect(mockSuccessRecoverDetail).toBeCalledTimes(1)
+      expect(mockSuccessRecoverDetail).toBeCalledWith(token)
+    })
+  })
+})
