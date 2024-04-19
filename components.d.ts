fde92ea0f81e73780af5236437923ff5e28fd811
zysim <9867871+zysim@users.noreply.github.com>
2024-04-19 21:50:04 +0800

    Add missing icons And some substitutes, really

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    ISvgBook: typeof import('~icons/svg/book')['default']
     ISvgCircleCheck: typeof import('~icons/svg/circle-check')['default']
     ISvgCircleExclamation: typeof import('~icons/svg/circle-exclamation')['default']
     ISvgCircleInfo: typeof import('~icons/svg/circle-info')['default']
@@ -14,6 +15,7 @@ declare module 'vue' {
     ISvgClose: typeof import('~icons/svg/close')['default']
     ISvgEyeHidden: typeof import('~icons/svg/eye-hidden')['default']
     ISvgEyeVisible: typeof import('~icons/svg/eye-visible')['default']
+    ISvgLink: typeof import('~icons/svg/link')['default']
     ISvgMenu: typeof import('~icons/svg/menu')['default']
     ISvgSearch: typeof import('~icons/svg/search')['default']
     ISvgSpinner: typeof import('~icons/svg/spinner')['default']
