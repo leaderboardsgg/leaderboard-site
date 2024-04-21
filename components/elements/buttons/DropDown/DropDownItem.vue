3c84af49c947c9d96e122fb96798bc67e10deda2
zysim <9867871+zysim@users.noreply.github.com>
2024-04-21 21:56:46 +0800

    Add event handling for DropDownItem

--- components/elements/buttons/DropDown/DropDownItem.vue
+++ components/elements/buttons/DropDown/DropDownItem.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts" />
+<script setup lang="ts">
+import type { SharedState } from './DropDown.vue'
+
+const sharedState: SharedState = inject('state', { active: ref(false) })
+
+const emit = defineEmits<{
+  (event: 'click'): void
+}>()
+
+function toggle() {
+  sharedState.active.value = false
+  emit('click')
+}
+</script>
 
 <template>
-  <button class="container">
+  <button class="container" @click="toggle">
     <slot />
   </button>
 </template>
@@ -9,8 +22,9 @@
 <style lang="postcss" scoped>
 .container {
   @apply py-2 text-center border-b border-gray-200 hover:bg-gray-100;
-}
-.container:last-child {
-  @apply border-b-0;
+
+  &:last-child {
+    @apply border-b-0;
+  }
 }
 </style>
