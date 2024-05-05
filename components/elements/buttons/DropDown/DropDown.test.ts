c724e4f6b4428eed1787daa2617501278689876b
zysim <9867871+zysim@users.noreply.github.com>
2024-05-05 17:28:21 +0800

    Add tests

--- components/elements/buttons/DropDown/DropDown.test.ts
+++ components/elements/buttons/DropDown/DropDown.test.ts
@@ -1,6 +1,7 @@
 import { enableAutoUnmount, mount } from '@vue/test-utils'
 import { getByTestId } from 'root/testUtils'
 import DropDown from './DropDown.vue'
+import DropDownItem from './DropDownItem.vue'
 import type { ComponentMountingOptions } from '@vue/test-utils'
 
 enableAutoUnmount(afterEach)
@@ -16,5 +17,29 @@ describe('<DropDown />', () => {
     })
     expect(wrapper.isVisible()).toBe(true)
     expect(getByTestId(wrapper, 'toggler').classes()).toContain('test')
+  })
+
+  describe('when the toggler is clicked', () => {
+    it('should render the slot item, then hide it on a second click', async () => {
+      const itemWrapper = mount(DropDownItem)
+
+      const wrapper = mountDropDown({
+        slots: { default: itemWrapper.html() },
+      })
+
+      await getByTestId(wrapper, 'toggler').trigger('click')
+      expect(wrapper.html()).toContain(itemWrapper.html())
+
+      await getByTestId(wrapper, 'toggler').trigger('click')
+      expect(wrapper.html()).not.toContain(itemWrapper.html())
+    })
+
+    it('should apply the style to the dropdown arrow', async () => {
+      const wrapper = mountDropDown()
+
+      await getByTestId(wrapper, 'toggler').trigger('click')
+
+      expect(getByTestId(wrapper, 'arrow').classes()).toContain('selected')
+    })
   })
 })
