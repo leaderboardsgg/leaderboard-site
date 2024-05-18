3a732a2b19950f6ec92d09ac1d5e5ee16223de7d
zysim <9867871+zysim@users.noreply.github.com>
2024-05-18 15:49:35 +0800

    Add token to endpoint call and create component folder

--- composables/api/useResendAccountConfirmation/index.ts
+++ composables/api/useResendAccountConfirmation/index.ts
@@ -1,25 +1,33 @@
 import {
+  useApi,
   type ApiResponse,
   type optionalParameters,
-  useApi,
 } from 'composables/useApi'
+import { useSessionToken } from 'composables/useSessionToken'
 import { Account } from 'lib/api/Account'
 
 /**
  * Resends the account confirmation email for a newly-registered user.
  */
-export async function useResendAccountConfirmation(
+export default async function useResendAccountConfirmation(
   opts: optionalParameters<void> = {},
 ): Promise<ApiResponse<void>> {
   const { onError, onOkay } = opts
+  const authToken = useSessionToken()
   const account = new Account({
     baseUrl: useRuntimeConfig().public.BACKEND_BASE_URL,
   })
 
-  return await useApi<void>(async () => await account.confirmCreate(), {
-    onError,
-    onOkay,
-  })
+  return await useApi<void>(
+    async () =>
+      await account.confirmCreate({
+        headers: {
+          Authorization: `Bearer ${authToken.value}`,
+        },
+      }),
+    {
+      onError,
+      onOkay,
+    },
+  )
 }
-
-export default useResendAccountConfirmation
