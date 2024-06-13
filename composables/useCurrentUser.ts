5b2a782257d8087479630649dc948cd028a47f1c
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-06-13 17:14:21 -0400

    Use a cookie to store the current user.

--- composables/useCurrentUser.ts
+++ composables/useCurrentUser.ts
@@ -1,10 +1,14 @@
+import { useCookie } from 'nuxt/app'
 import type { UserViewModel } from 'lib/api/data-contracts'
+import type { CookieRef } from 'nuxt/app'
 
-export function useCurrentUser() {
-  return useState<UserViewModel>('current_user', () => ({
-    id: '',
-    username: '',
-  }))
+export function useCurrentUser(): CookieRef<UserViewModel> {
+  return useCookie('current_user', {
+    default: () => ({
+      id: '',
+      username: '',
+    }),
+  })
 }
 
 export default useCurrentUser
