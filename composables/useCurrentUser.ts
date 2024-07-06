bf4f4ee0f938378abea6100e08aff732888aa5d4
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-07-06 16:46:17 -0400

    Use an empty object to signify no current user.

--- composables/useCurrentUser.ts
+++ composables/useCurrentUser.ts
@@ -1,12 +1,14 @@
 import { Users } from 'lib/api/Users'
 import type { UserViewModel } from 'lib/api/data-contracts'
 
-export const useCurrentUser = () =>
-  useAsyncData(
+export default function useCurrentUser() {
+  return useAsyncData<Partial<UserViewModel>>(
     'current_user',
     async () => {
       const token = useSessionToken().value
-      if (!token) return undefined
+      if (!token) {
+        return {}
+      }
 
       const users = new Users({
         baseUrl: useRuntimeConfig().public.backendBaseUrl,
@@ -19,9 +21,10 @@ export const useCurrentUser = () =>
           }),
       )
 
-      return resp.data
+      return resp.data ?? {}
     },
     {
       watch: [useSessionToken()],
     },
   )
+}
