3e4084ed2de950393e23757501bad55536df03da
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-07-06 15:43:34 -0400

    formatting

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,12 +1,11 @@
-/* eslint-disable vue/sort-keys */
 // TODO: This should be uncommented after 8.0.0-beta.11
 import { localeMessages, supportedLocales } from './configUtils'
 
 export default defineI18nConfig(() => ({
+  availableLocales: supportedLocales,
   fallbackLocale: 'en',
   legacy: false,
   locale: 'en',
   // TODO: can this be cleaner via some sort of iteration? (not sure)
   messages: localeMessages,
-  availableLocales: supportedLocales,
 }))
