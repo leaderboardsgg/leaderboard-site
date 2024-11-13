cf1a6fe600cb5205e5a24c2def30eba979555b5e
Ted Wollman <25165500+TheTedder@users.noreply.github.com>
2024-11-13 15:37:58 -0500

    Mock local storage when testing useLogoutUser.

--- composables/api/useLogoutUser/useLogoutUser.test.ts
+++ composables/api/useLogoutUser/useLogoutUser.test.ts
@@ -2,13 +2,24 @@ import { useCurrentUser, useSessionToken } from '#imports'
 import type { UserViewModel } from 'lib/api/data-contracts'
 import useLogoutUser from '.'
 
-const token = 'auth-token'
+let token = ''
 const user: UserViewModel = {
   id: '575888bd-9702-41a9-9b75-fc87d785c22a',
   role: 'Confirmed',
   username: 'test',
   createdAt: '1984-01-01T00:00:00Z',
 }
+
+vi.mock('@vueuse/core', () => ({
+  useLocalStorage: () => ({
+    get value() {
+      return token
+    },
+    set value(val) {
+      token = val
+    },
+  }),
+}))
 
 afterEach(() => {
   fetchMock.resetMocks()
@@ -19,14 +30,14 @@ afterEach(() => {
 describe('useLogoutUser', () => {
   beforeEach(() => {
     fetchMock.mockIf(/.*\/[Uu]ser\/me/, () => JSON.stringify(user))
-    useSessionToken().value = token
+    token = 'auth-token'
   })
 
   it('sets the `authToken` and `currentUser` back to the default values', async () => {
     const authToken = useSessionToken()
     const { data, refresh } = await useCurrentUser()
 
-    expect(authToken.value).toEqual(token)
+    expect(authToken.value).toEqual('auth-token')
     expect(data.value).toEqual(user)
 
     useLogoutUser()
