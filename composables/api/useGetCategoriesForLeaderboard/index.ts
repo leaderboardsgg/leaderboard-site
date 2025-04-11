f781a3e0b00f484492b67403e146f783945bff63
zysim <9867871+zysim@users.noreply.github.com>
2025-04-12 00:16:03 +0800

    Add useGetCategoriesForLeaderboard composable

---
+++ composables/api/useGetCategoriesForLeaderboard/index.ts
@@ -0,0 +1,34 @@
+import { useRuntimeConfig } from '#app'
+import { ref } from 'vue'
+import useApi, { type optionalParameters } from '~/composables/useApi'
+import { Categories } from '~/lib/api/Categories'
+import type {
+  CategoryViewModelListView,
+  GetCategoriesForLeaderboardParams,
+} from '~/lib/api/data-contracts'
+
+export default async function useGetCategoriesForLeaderboard(
+  opts: optionalParameters<CategoryViewModelListView> = {},
+  params: GetCategoriesForLeaderboardParams,
+) {
+  const { onError, onOkay } = opts
+  const responseData = ref<CategoryViewModelListView>({
+    data: [],
+    limitDefault: 0,
+    limitMax: 0,
+    total: 0,
+  })
+
+  const client = new Categories({
+    baseUrl: useRuntimeConfig().public.backendBaseUrl,
+  })
+
+  return useApi<CategoryViewModelListView>(
+    async () => client.getCategoriesForLeaderboard(params),
+    {
+      onError,
+      onOkay,
+      responseData,
+    },
+  )
+}
