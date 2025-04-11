10beaed06c5dacae3fbebc3351362c27cf19859c
zysim <9867871+zysim@users.noreply.github.com>
2025-04-12 01:00:47 +0800

    Add useGetCategoryBySlug

---
+++ composables/api/useGetCategoryBySlug/index.ts
@@ -0,0 +1,40 @@
+import { useRuntimeConfig } from '#app'
+import { ref } from 'vue'
+import useApi, { type optionalParameters } from '~/composables/useApi'
+import { Categories } from '~/lib/api/Categories'
+import type {
+  CategoryViewModel,
+  GetCategoryBySlugParams,
+} from '~/lib/api/data-contracts'
+
+export default async function useGetCategoryBySlug(
+  params: GetCategoryBySlugParams,
+  opts: optionalParameters<CategoryViewModel> = {},
+) {
+  const { onError, onOkay } = opts
+  const responseData = ref<CategoryViewModel>({
+    id: -1,
+    name: '',
+    slug: '',
+    info: '',
+    type: 'Time',
+    sortDirection: 'Ascending',
+    leaderboardId: -1,
+    createdAt: '',
+    updatedAt: null,
+    deletedAt: null,
+  })
+
+  const client = new Categories({
+    baseUrl: useRuntimeConfig().public.backendBaseUrl,
+  })
+
+  return useApi<CategoryViewModel>(
+    async () => client.getCategoryBySlug(params),
+    {
+      onError,
+      onOkay,
+      responseData,
+    },
+  )
+}
