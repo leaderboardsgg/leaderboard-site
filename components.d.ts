f1caa788cf1d1110e0f47c6e0191c0892ca2ed7e
Daniel Turcich <github@danielTurcich.com>
2025-08-02 23:56:57 -0400

    test pnpm dedupe && pinning action versions

--- components.d.ts
+++ components.d.ts
@@ -7,7 +7,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    ISvgBook: typeof import('~icons/svg/book')['default']
     ISvgCircleCheck: typeof import('~icons/svg/circle-check')['default']
     ISvgCircleExclamation: typeof import('~icons/svg/circle-exclamation')['default']
     ISvgCircleInfo: typeof import('~icons/svg/circle-info')['default']
@@ -15,7 +14,6 @@ declare module 'vue' {
     ISvgClose: typeof import('~icons/svg/close')['default']
     ISvgEyeHidden: typeof import('~icons/svg/eye-hidden')['default']
     ISvgEyeVisible: typeof import('~icons/svg/eye-visible')['default']
-    ISvgLink: typeof import('~icons/svg/link')['default']
     ISvgMenu: typeof import('~icons/svg/menu')['default']
     ISvgSearch: typeof import('~icons/svg/search')['default']
     ISvgSpinner: typeof import('~icons/svg/spinner')['default']
