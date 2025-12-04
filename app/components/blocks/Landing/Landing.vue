3b6c19f1e9e959a4e96d4cd13d50322ee33deb77
pierreoa <pierreomar@gmail.com>
2025-12-04 08:04:13 +0100

    feat: add runStats to leaderboards and make grid css dynamic

--- app/components/blocks/Landing/Landing.vue
+++ app/components/blocks/Landing/Landing.vue
@@ -2,18 +2,17 @@
 import LandingLeaderboards from './LandingLeaderboards.vue'
 import LandingAbout from './LandingAbout.vue'
 import { useGetLeaderboards } from '~/composables/api'
-
 import Loader from 'blocks/Loader/Loader.vue'
 
-const leaderboards = await useGetLeaderboards()
+const { data: leaderboards, loading } = await useGetLeaderboards()
 </script>
 
 <template>
   <div>
     <h2 class="mb-4 text-3xl font-bold">Games</h2>
     <div class="flex flex-col lg:flex-row">
-      <Loader v-if="leaderboards.loading"></Loader>
-      <LandingLeaderboards :leaderboards="leaderboards.data?.data || []" />
+      <Loader v-if="loading"></Loader>
+      <LandingLeaderboards :leaderboards="leaderboards?.data || []" />
       <LandingAbout />
     </div>
   </div>
