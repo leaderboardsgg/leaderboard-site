d9e82729cd8a96d77080d5f5a5a5073e0cbf2b47
zysim <9867871+zysim@users.noreply.github.com>
2025-12-06 23:50:40 +0800

    Drop rules section

--- app/components/blocks/RunsPage/CategoryInfo/CategoryInfo.vue
+++ app/components/blocks/RunsPage/CategoryInfo/CategoryInfo.vue
@@ -14,14 +14,8 @@ defineProps<CategoryInfoProps>()
     <p class="text-xs">
       {{ category.info || '-' }}
     </p>
-    <h3 class="mt-6 font-medium">Run Type Rules</h3>
-    <p class="text-xs">
-      {{ category.info || '-' }}
-    </p>
     <!-- TODO: Create Create Run page and add link to that page -->
-    <ButtonLink
-      class="mt-12 w-fit rounded border border-yellow-400 px-9 py-3 text-xl font-semibold capitalize"
-    >
+    <ButtonLink class="mt-12 w-fit rounded border border-yellow-400 px-9 py-3 text-xl font-semibold capitalize">
       Submit A Run
     </ButtonLink>
   </div>
