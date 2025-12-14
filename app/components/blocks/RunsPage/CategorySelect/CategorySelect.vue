f1ea79b4ae52ad749cd589cda15b278ef18c966c
Shep <hurlingtriage@protonmail.com>
2025-12-14 10:33:12 +0800

    Split category info out of CategorySelect

--- app/components/blocks/RunsPage/CategorySelect/CategorySelect.vue
+++ app/components/blocks/RunsPage/CategorySelect/CategorySelect.vue
@@ -4,38 +4,26 @@ import type { CategoryViewModel } from '~~/lib/api/data-contracts'
 
 interface CategorySelect {
   categories: CategoryViewModel[]
-  activeCategory: CategoryViewModel
+  activeCategorySlug: string
 }
 
 const props = defineProps<CategorySelect>()
-
-function isActiveCategorySlug(slug: string) {
-  return props.activeCategory.slug.toLocaleLowerCase() === slug
-}
 </script>
 
 <template>
   <div class="grid grid-cols-2 gap-6 p-6">
     <div class="flex gap-3">
       <ButtonLink
-        v-for="{ name, slug, id } of categories"
+        v-for="{ name, slug, id } of props.categories"
         :key="id"
         :name="name"
-        :to="`?category=${slug}`"
-        :class="{ active: isActiveCategorySlug(slug) }"
+        :to="`#${slug}`"
+        active-class="active"
         class="border border-white px-6 py-3 text-xs text-white"
       >
         {{ name }}
       </ButtonLink>
     </div>
-    <div class="justify-self-end">Run Type: {{ activeCategory?.type }}</div>
-    <aside class="col-span-2">
-      <h2 class="text-xl">Category Description</h2>
-      <span>{{ activeCategory?.info || '-' }}</span>
-    </aside>
-    <ButtonLink class="w-fit !bg-white !text-black">
-      Submit your run!
-    </ButtonLink>
   </div>
 </template>
 
