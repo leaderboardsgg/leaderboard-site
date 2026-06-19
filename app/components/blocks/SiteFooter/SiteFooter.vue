ff4e7c6aa3a70594ce330d387fb1b186752ac414
Daniel Turcich <github@danielTurcich.com>
2026-06-19 01:32:11 -0400

    format fix

--- app/components/blocks/SiteFooter/SiteFooter.vue
+++ app/components/blocks/SiteFooter/SiteFooter.vue
@@ -6,7 +6,9 @@ const currentYear = computed(() => new Date().getFullYear())
 </script>
 
 <template>
-  <footer class="grid md:grid-cols-3 grid-cols-1 md:gap-y-0 gap-y-5 items-center rounded bg-gray-100 p-5 text-gray-600 mt-auto">
+  <footer
+    class="grid md:grid-cols-3 grid-cols-1 md:gap-y-0 gap-y-5 items-center rounded bg-gray-100 p-5 text-gray-600 mt-auto"
+  >
     <p class="md:col-start-2 w-full text-center">Copyright {{ currentYear }}</p>
     <LanguageSelector data-testid="language-selector" />
   </footer>
